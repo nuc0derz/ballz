c53336ee51331cac01c1f9f4290ff0f093498edf
Phillip Landers <planders.tech@gmail.com>
2022-09-06 09:06:38 -0400

    added test touch controls

--- canvas.js
+++ canvas.js
@@ -124,7 +124,7 @@ canvas.addEventListener("mousemove", (event) => {
   mouse.y = event.y;
 });
 canvas.addEventListener("touchmove", (event) => {
-  e.preventDefault();
+  event.preventDefault();
   mouse.x = event.x - 120;
   mouse.y = event.y;
 });
